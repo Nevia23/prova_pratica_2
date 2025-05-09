4b6fe13b2fb00c717a84317d865115077536e300
nicholas <nicholasmoretto31@gmail.com>
2025-05-09 11:01:28 +0200

    Closes #6: predisposto codice iniziale per RomanPrinter

---
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Bianca Zaghetto 1167741
+// Nicholas Moretto 1226318
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    public static String print(int num){
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+    private static String printAsciiArt(String romanNumber){
+        //TODO
+        return null;
+    }
+}
