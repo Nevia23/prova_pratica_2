691cdea1f6175336d415301f7ba7fc2732bb571d
nicholas <nicholasmoretto31@gmail.com>
2025-05-09 11:23:56 +0200

    Closes #4: sviluppo di RomanPrinter

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -5,11 +5,113 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
+
+    // ASCII Art per le lettere romane
+    private static final String[] I = {
+        " _____  ",
+        "|_   _| ",
+        "  | |   ",
+        "  | |   ",
+        " _| |_  ",
+        "|_____| "
+    };
+
+    private static final String[] V = {
+        "__      __",
+        "\\ \\    / /",
+        " \\ \\  / / ",
+        "  \\ \\/ /  ",
+        "   \\  /   ",
+        "    \\/    "
+    };
+
+    private static final String[] X = {
+        "__   __",
+        "\\ \\ / /",
+        " \\ V / ",
+        "  > <  ",
+        " / . \\ ",
+        "/_/ \\_\\"
+    };
+
+    private static final String[] L = {
+        " _       ",
+        "| |      ",
+        "| |      ",
+        "| |      ",
+        "| |____  ",
+        "|______| "
+    };
+
+    private static final String[] C = {
+        "  _____ ",
+        " / ____|",
+        "| |     ",
+        "| |     ",
+        "| |____ ",
+        " \\_____|"
+    };
+
+    private static final String[] D = {
+        " _____  ",
+        "|  __ \\ ",
+        "| |  | |",
+        "| |  | |",
+        "| |__| |",
+        "|_____/ "
+    };
+
+    private static final String[] M = {
+        " __  __ ",
+        "|  \\/  |",
+        "| \\  / |",
+        "| |\\/| |",
+        "| |  | |",
+        "|_|  |_|"
+    };
+
     public static String print(int num){
         return printAsciiArt(IntegerToRoman.convert(num));
     }
     private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
+        StringBuilder[] righe = new StringBuilder[6];
+        for (int i = 0; i < 6; i++) {
+            righe[i] = new StringBuilder();
+        }
+
+        for (char c : romanNumber.toCharArray()) {
+            String[] lettera = getAsciiArt(c);
+            for (int i = 0; i < 6; i++) {
+                righe[i].append(lettera[i]).append(" ");
+            }
+        }
+
+        StringBuilder risultato = new StringBuilder();
+        for (StringBuilder riga : righe) {
+            risultato.append(riga.toString().stripTrailing()).append("\n");
+        }
+
+        return risultato.toString();
+    }
+
+    private static String[] getAsciiArt(char c) {
+        switch (c) {
+            case 'I':
+                return I;
+            case 'V':
+                return V;
+            case 'X':
+                return X;
+            case 'L':
+                return L;
+            case 'C':
+                return C;
+            case 'D':
+                return D;
+            case 'M':
+                return M;
+            default:
+                throw new IllegalArgumentException("Carattere non valido: " + c);
+        }
     }
 }
